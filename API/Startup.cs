0753e10cf3521be10ad198653cfd6089c02c1dcc
ArranBell123 <arran.bell:outlook.com>
2021-12-16 15:31:59 +0000

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,9 +23,7 @@ namespace API
         {
             _config = config;
         }
-
-
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -40,6 +38,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +54,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 
             app.UseAuthorization();
 
